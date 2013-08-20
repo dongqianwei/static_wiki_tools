f2c67efcc2e686daec19ce75658964543929c1d1
dongqianwei <dongqianwei@foxmail.com>
2013-08-21 01:00:44 +0800

    format feature

--- WIKI-Tools/bin/wiki.pl
+++ WIKI-Tools/bin/wiki.pl
@@ -2,10 +2,28 @@ use v5.16;
 use autodie;
 use File::Slurp;
 use File::Basename;
+use Text::Template;
 use constant {
         SRCDIR => './src/',
         TARDIR => './target/'
     };
+
+my %specTokens = (
+        '#'   => 'h1',
+        '##'  => 'h2',
+        '###' => 'h3',
+        '___' => 'line',
+);
+
+my $headRe = qr/^(#+|___)\s*/;
+
+my %templateMap = (
+                   h1 => Text::Template->new(TYPE=>'STRING',SOURCE=>'<h1>{$line}</h1>'),
+                   h2 => Text::Template->new(TYPE=>'STRING',SOURCE=>'<h2>{$line}</h2>'),
+                   h3 => Text::Template->new(TYPE=>'STRING',SOURCE=>'<h3>{$line}</h3>'),
+                   line => Text::Template->new(TYPE=>'STRING',SOURCE=>'<hr/>'),
+                   section => Text::Template->new(TYPE=>'STRING',SOURCE=>'<p>{$line}</p>'),
+                   );
 
 mkdir SRCDIR unless -d SRCDIR;
 mkdir TARDIR unless -e TARDIR;
@@ -16,12 +34,45 @@ while (@files) {
     my $fname = shift @files;
     open(my $sfd, '<', $fname);
     open(my $tfd, '>', TARDIR.scalar fileparse $fname =~ s/md$/html/xmsr);
+
+    my $sectionMark;
+    my @sectionStack;
     while (<$sfd>) {
         chomp;
         my $line = $_;
 
+        my $tLine;
         my @terms = $line =~ m/\[([^\]]*?)\]/xmsg;
-        say $tfd $line =~ s|\[([^\]]*?)\]|<a href="./$1.html">$1</a>|mxsrg;
+
+        #process line prefix
+        my ($mode) = $line =~ $headRe;
+        say "mode $mode; line: $line";
+        if ($mode and !$sectionMark) {
+            my $temp = $templateMap{$specTokens{$mode}};
+            $tLine = $line =~ s/$headRe//r;
+            $tLine = $temp->fill_in(HASH => {line => $tLine});
+        }
+        else {
+                $tLine = $line;
+                $sectionMark = 1 unless $line =~ m/^$/msx;
+        }
+
+        #process terms
+        $tLine =~ s|\[([^\]]*?)\]|<a href="./$1.html">$1</a>|mxsg if @terms;
+        $sectionMark = 0 if $tLine =~ m/^$/xsm;
+        say "blank line $." if $tLine =~ m/^$/xsm;;
+        if ($sectionMark) {
+                push @sectionStack, $tLine;
+        }
+        else {
+            if (@sectionStack) {
+                say $tfd $templateMap{section}->fill_in(HASH => {line => join ' ', @sectionStack});
+                @sectionStack = ();
+            }
+            else {
+                say $tfd $tLine;
+            }
+        }
 
         for my $term (@terms) {
             if (not -e $srcSet{$term.'.md'}) {
@@ -31,5 +82,6 @@ while (@files) {
         }
     }
     close $sfd;
+    say $tfd $templateMap{section}->fill_in(HASH => {line => join ' ', @sectionStack}) if @sectionStack;
     close $tfd;
 }
